ef71ec699c725f71075d8a04193dc0df4626c2ff
Alex Church <ac14293@bristol.ac.uk>
2021-04-01 17:14:14 +0100

    add ycb init and prototype files

--- pybullet_object_models/ycb_objects/__init__.py
+++ pybullet_object_models/ycb_objects/__init__.py
@@ -4,3 +4,11 @@ import os
 def getDataPath():
   resdir = os.path.join(os.path.dirname(__file__))
   return resdir
+
+def getModelList():
+    data_path = getDataPath()
+    model_list = [os.path.basename(os.path.normpath(f.path)) for f in os.scandir(data_path) if f.is_dir()]
+    try:
+        model_list.remove('__pycache__')
+    except:pass
+    return model_list
